4b3d7f63da2e9a296341b417cf7d84abbcb15691
Danilitarodri05 <danielitaena21@gmail.com>
2024-11-08 20:47:30 -0600

    Create OPERACIONES.java

---
+++ OPERACIONES.java
@@ -0,0 +1,14 @@
+
+public class MainOperacionesArirmeticas
+ {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 4;
+
+        System.out.println("Suma: " + (num1 + num2));
+        System.out.println("Resta: " + (num1 - num2));
+        System.out.println("Multiplicación: " + (num1 * num2));
+        System.out.println("División: " + (num1 / num2));
+        System.out.println("Módulo: " + (num1 % num2));
+    }
+}
