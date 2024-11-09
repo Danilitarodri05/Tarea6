f96096b3daf9fea3d3821fc2ef9192417384467b
Danilitarodri05 <danielitaena21@gmail.com>
2024-11-08 20:48:46 -0600

    Create ARREGLO.java

---
+++ ARREGLO.java
@@ -0,0 +1,9 @@
+public class MainArreglo {
+    public static void main(String[] args) {
+        String[] compañeros = {"keysi", "Arnol", "Danlis", "Evelyn", "Felipe", "Jeyson", "Andres", "Blanca", "Isaac", "Javie"};
+
+        for (String nombre : compañeros) {
+            System.out.println(nombre);
+        }
+    }
+}
