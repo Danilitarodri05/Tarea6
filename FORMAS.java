ae2e80b14676f2406fba9e4b4d1ab5d8d976b4c8
Danilitarodri05 <danielitaena21@gmail.com>
2024-11-08 21:01:15 -0600

    Create FORMAS.java

---
+++ FORMAS.java
@@ -0,0 +1,12 @@
+
+public abstract class Formas {
+    protected String color;  
+
+
+    public void establecerColor(String color) {
+        this.color = color;
+    }
+
+ 
+    public abstract void dibujar();
+}
